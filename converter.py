ed7ea28d0362a3dc21a1253b1c226000962ac368
Lucas Goudin <theprocraft@gmail.com>
2018-10-07 15:08:28 +0200

    Added UPC-A conversion

--- converter.py
+++ converter.py
@@ -1,6 +1,6 @@
 from PIL import Image
 
-codePatterns = {    " ":"11011001100",
+codePatterns128 = {    " ":"11011001100",
                     "0":"10011101100",
                     "1":"10011100110",
                     "2":"11001110010",
@@ -39,28 +39,60 @@ codePatterns = {    " ":"11011001100",
                     "Z":"11101100010",
                     }
 
-quietZone = "0000000000"
-startCode = "11010010000"
-stopCode = "1100011101011"
+codePatternsL = ["0001101","0011001","0010011","0111101","0100011","0110001","0101111","0111011","0110111","0001011"]
+codePatternsR = ["1110010","1100110","1101100","1000010","1011100","1001110","1010000","1000100","1001000","1110100"]
+quietZone = "000000000"
+startZone = "101"
+middleZone = "01010"
+endZone = "101"
 
-barCode = "BARCODE"
+quietZone128 = "0000000000"
+startCode128 = "11010010000"
+stopCode128 = "1100011101011"
+
+barCode128 = "BARCODE"
+barCode = "11111111111111111"
 
 def convertCode(code):
-    global quietZone, startCode, stopCode
-    res = quietZone + startCode
-    for i in code:
-        res += codePatterns[i]
-    res += stopCode + quietZone
+    codeL = code[:len(code)//2]
+    codeR = code[len(code)//2:]
+    res = quietZone + startZone
+    for i in codeL:
+        res += codePatternsL[int(i)]
+    res += middleZone
+    for j in codeR:
+        res += codePatternsR[int(j)]
+    res += endZone + quietZone
     return res
 
-
-def printBarCode128(code, height):
+def printBarCode(code, height):
     code = convertCode(code)
-    barcode = Image.new("1",(len(code),height+20),1)
+    barcode = Image.new("1",(len(code),height),1)
     for y in range(height):
             for x in range(len(code)):
                 barcode.putpixel((x,y),0) if code[x]=="1" else barcode.putpixel((x,y),1)
     
     barcode.show()
 
-printBarCode128(barCode,10)
+printBarCode(barCode,10)
+
+def convertCode128(code):
+    global quietZone128, startCode128, stopCode128
+    res = quietZone128 + startCode128
+    for i in code:
+        res += codePatterns128[i]
+    res += stopCode128 + quietZone128
+    return res
+
+
+def printBarCode128(code, height):
+    code = convertCode128(code)
+    barcode = Image.new("1",(len(code),height),1)
+    for y in range(height):
+            for x in range(len(code)):
+                barcode.putpixel((x,y),0) if code[x]=="1" else barcode.putpixel((x,y),1)
+    
+    barcode.show()
+    
+
+
